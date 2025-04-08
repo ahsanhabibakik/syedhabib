51ed6373c7cc0fb6549a825bff5610591627ece2
Syed Mir Habib <mirakik.habib@gmail.com>
2025-04-09 04:06:38 +0600

    install gray-matter to prepare for dynamic content and markdown blogs

---
+++ src/app/content/siteConfig.ts
@@ -0,0 +1,13 @@
+// content/siteConfig.ts
+const siteConfig = {
+  name: "Syed Mir Ahsan Habib",
+  shortName: "Akik",
+  email: "syedmirhabib@gmail.com",
+  social: {
+    twitter: "https://twitter.com/syedmirhabib",
+    github: "https://github.com/syedmirhabib",
+    linkedin: "https://linkedin.com/in/syedmirhabib",
+  },
+};
+
+export default siteConfig;
