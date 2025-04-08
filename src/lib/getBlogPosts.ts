7e7b169232d76a6270631b07560155896b132712
Syed Mir Habib <mirakik.habib@gmail.com>
2025-04-09 04:01:34 +0600

    Blog from Markdown (or CMS later)

---
+++ src/lib/getBlogPosts.ts
@@ -0,0 +1,23 @@
+// lib/getBlogPosts.ts
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'src/content/blog');
+
+export function getAllPosts() {
+  const filenames = fs.readdirSync(postsDirectory);
+
+  return filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const { data } = matter(fileContents);
+
+    return {
+      slug: filename.replace('.md', ''),
+      title: data.title,
+      excerpt: data.excerpt,
+      date: data.date,
+    };
+  });
+}
